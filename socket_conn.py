3df0154bb22689185174cac5fc311afa2299b24e
Aleksei <93991616+AlekseyPeng@users.noreply.github.com>
2022-10-30 20:58:59 +0800

    Add files via upload

---
+++ socket_conn.py
@@ -0,0 +1,53 @@
+import socketserver
+from time import sleep
+from secret import flag
+import signal
+
+
+from Crypto.Util.number import *
+import random
+
+
+class Task(socketserver.BaseRequestHandler):
+    def _recvall(self):
+        BUFF_SIZE = 2048
+        data = b''
+        while True:
+            part = self.request.recv(BUFF_SIZE)
+            data += part
+            if len(part) < BUFF_SIZE:
+                break
+        return data.strip()
+
+    def send(self, msg, newline=True):
+        try:
+            if newline:
+                msg += b'\n'
+            self.request.sendall(msg)
+        except:
+            pass
+
+    def recv(self):
+        return self._recvall()
+        
+
+
+
+    def handle(self):
+        border = b"|"
+        self.send(border*72)
+        
+class ThreadedServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+class ForkedServer(socketserver.ForkingMixIn, socketserver.TCPServer):
+    pass
+
+
+if __name__ == "__main__":
+    HOST, PORT = '0.0.0.0', 10001
+    server = ForkedServer((HOST, PORT), Task)
+    server.allow_reuse_address = True
+    print(HOST, PORT)
+    server.serve_forever()
